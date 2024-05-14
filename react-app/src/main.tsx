e3d4fb58e9a8dcc74479c2be3b7e77d98c5d0a8e
jung-ah <a@jung-ah-MacBook-Pro.local>
2024-05-14 18:10:21 +0900

    /restaurant API

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,5 +1,21 @@
-function main() {
-  // TODO: App 컴포넌트를 render 해주세요.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+async function main() {
+  const element = document.getElementById('root');
+
+  if (!element) {
+    return;
+  }
+
+  const root = ReactDOM.createRoot(element);
+
+  root.render((
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ));
 }
 
 main();
